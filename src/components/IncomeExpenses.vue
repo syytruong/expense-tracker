e9e78cfa1241666aa302c0a41bc28437df56ba1f
Sy Truong <sy.truong@detectorinspector.com.au>
2023-11-10 17:08:00 +0700

    FEAT: complete simple project to use in local

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" class="money plus">+${{ income }}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minus" class="money minus">-${{ expenses }}</p>
+      </div>
+    </div>
+
+    <div>
+      <div>
+        <h4>Balance</h4>
+        <p id="money-plus" class="money plus">+${{ income }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    income: {
+      type: Number,
+      required: true,
+    },
+    expenses: {
+      type: Number,
+      required: true,
+    },
+  });
+  </script>
